f667a9792e9996e95b63d1021f895441689b9711
Kushagra Garg <kushagra.garg1291@gmail.com>
2020-04-03 04:08:05 +0530

    payments pending again

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -5,7 +5,7 @@ ENVIRONMENT = os.getenv('ENVIRONMENT', 'development')
 DEBUG = True
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = '-05sgp9!deq=q1nltm@^^2cc+v29i(tyybv3v2t77qi66czazj'
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['djecommerce-shop.herokuapp.com', '127.0.0.1']
 
 INSTALLED_APPS = [
     'django.contrib.admin',
