257ddbc6033dfb47236a5ad4a07fdd4a933f9ec1
Kushagra Garg <kushagra.garg1291@gmail.com>
2020-04-05 03:38:17 +0530

    static files settings

--- djecommerce/settings.py
+++ djecommerce/settings.py
@@ -67,10 +67,10 @@ USE_I18N = True
 USE_L10N = True
 USE_TZ = True
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+# STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
